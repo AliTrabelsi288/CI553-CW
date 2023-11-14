72d29fa6b3b6500c632d2da22bde75b732f59d50
alitrabelsi <alitrabelsi@alis-air.home>
2023-11-14 10:21:47 +0000

    Added first JUnit class

---
+++ clients/shopDisplay/DisplayViewTest.java
@@ -0,0 +1,44 @@
+package clients.shopDisplay;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DisplayViewTest {
+
+	@Test
+	void testDisplayView() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateObservableObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateGraphics() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPaintGraphics() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDrawScreen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDrawActualScreen() {
+		fail("Not yet implemented");
+	}
+
+}
